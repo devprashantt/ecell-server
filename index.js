db55829f64aa6512d8a9be5cc8d90791cb28152f
Prashant Kumar Singhcd <pk3076889@gmail.com>
2023-06-22 02:18:36 +0530

    CORS

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const allowedOrigins = ['http://127.0.0.1:5173', '"https://ecelliiit.netlify.app
 
 app.use(cors(
     {
-        origin: allowedOrigins,
+        origin: process.env.CLIENT_URL,
         credentials: true
     }
 ));
